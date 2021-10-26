99c48982376e99d2a4562fadd2314ce3e748dfc6
Tomas Jemelka <jemelkat.dev@gmail.com>
2021-10-26 18:14:14 +0200

    Init commit

---
+++ src/main/java/com/aspicereporting/AspiceReportingApplication.java
@@ -0,0 +1,13 @@
+package com.aspicereporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspiceReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspiceReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspicereporting/AspiceReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspicereporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspiceReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
